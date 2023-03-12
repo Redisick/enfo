7ca3e7b5327a4e040d6d9096dc99f555de035d7f
Redisick <110599744+Redisick@users.noreply.github.com>
2023-03-12 22:07:31 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,23 @@
+const TOKEN = "6235379673:AAF-TjGqKIvp6M-SJeSk9Co9KsWFlRfhQDM";
+
+const bot = new Bot(TOKEN, 946874164);
+
+bot.getUpdates()
+    .then(res => {
+        console.log(res.result);
+    })
+ 
+document.getElementById("form").addEventListener("submit", e => {
+    e.preventDefault();
+
+    const text = "Имя: " +
+                document.getElementById("nameInput").value +
+                "%0A" +
+                "Номер или ссылка: " +
+                document.getElementById("numberInput").value +
+                "%0A" +
+                "Комментарий: " +
+                document.getElementById("commentInput").value;
+
+    bot.sendMessage(text);
+})
